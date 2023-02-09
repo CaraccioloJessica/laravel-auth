e6595ee7945f1b71655a1ef9f8c7b6621656802c
Jessica <jesscara.993@gmail.com>
2023-02-09 12:17:18 +0100

    mb card changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 <h2 class="text-center my-3">I miei progetti</h2>
 <div class="container d-flex flex-wrap">
     @foreach ($projects as $project)
-        <div class="card mb-2 mx-2" style="width: 16rem;">
+        <div class="card mb-3 mx-2" style="width: 16rem;">
           <h4 class="mb-2">{{ $project -> name }}</h4>
           <div class="mb-2">{{ $project -> description }}</div>
           <img  class="mb-2" src="{{ $project -> main_image }}" alt="">
